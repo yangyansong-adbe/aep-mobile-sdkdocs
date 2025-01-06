a0def6ece59f0c17dcbbb08b7c3b1bc5a2a0d470
Yansong Yang <yanyang@adobe.com>
2025-01-06 12:35:17 -0600

    Update index.js

--- .github/scripts/release_notes/index.js
+++ .github/scripts/release_notes/index.js
@@ -25,13 +25,11 @@ if (GITHUB_TOKEN === undefined) {
 
 const DRY_RUN = process.argv.includes("--dry-run")
 
-// Before running the script, make sure the default time zone is set to PST in the GitHub action
-// TODO: For some reason, the code below to update the timezone of the Github Action runners is not working. Ignore this step for now and will fix it later.
+
 const offset = new Date().getTimezoneOffset()
-console.log(offset);
-// if (!setTimeZoneToPST()) {
-//     throw new Error("The default time zone is not set to PST")
-// }
+console.log(`Timezone offset is: ${offset}`);
+const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+console.log(`Timezone is: ${timezone}`); 
 
 console.log(`Start to fetch release info from GitHub created after [${convertToDateTime(timestampObj.ts)}]`);
 
